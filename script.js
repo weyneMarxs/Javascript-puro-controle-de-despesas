eafa033513771ca1d02b340813da49b29117e1bf
weyne <weyneace@gmail.com>
2023-01-08 08:58:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+const transactionsUl = document.querySelector('#transactions')
+const dummyTransactions = [
+  {
+    id: 1,
+    name: 'bolo',
+    amount: -20
+  },
+  {
+    id: 2,
+    name: 'salário',
+    amount: 300
+  },
+  {
+    id: 3,
+    name: 'torta de frango',
+    amount: -10
+  },
+  {
+    id: 4,
+    name: 'violão',
+    amount: 150
+  }
+]
+const addTransactionIntoDOM = transaction => {
+  const operator = transaction.amount < 0 ? '-' : '+'
+  const cssClass = transaction.amount < 0 ? 'minus' : 'plus'
+  const amountWithoutOperator = Math.abs(transaction.amount)
+  const li = document.createElement('li')
+
+  li.classList.add(cssClass)
+  li.innerHTML = `${transaction.name} <span>${operator} R$${amountWithoutOperator}</span><button class="delete-btn">x</button>`
+  transactionsUl.append(li)
+}
+const updateBalanceValues = () => {
+  const transactionsAmount = dummyTransactions.map(
+    transaction => transaction.amount
+  )
+  const total = transactionsAmount
+    .reduce((accumulator, transaction) => accumulator + transaction, 0)
+    .toFixed(2)
+  const income = transactionsAmount
+    .filter(value => value > 0)
+    .reduce((accumulator, value) => accumulator + value, 0)
+}
+const init = () => {
+  dummyTransactions.forEach(addTransactionIntoDOM)
+  updateBalanceValues()
+}
+
+init()
